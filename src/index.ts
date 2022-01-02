bf153b54de799b4b573035c2c23a7d81663aab39
marcelogaldino <marcelinhomgo@gmail.com>
2022-01-02 14:52:15 -0300

    feat: filename receives original title from video

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ app.get('/', (request: Request, response: Response) => {
 
 app.post('/download', async (request: Request, response: Response) => {
     const { url } = request.body
-    console.log(request.body)
     // response.header("Content-Disposition", 'attachment;\  filename="Video.mp4');
 
     if (!url) {
@@ -22,14 +21,34 @@ app.post('/download', async (request: Request, response: Response) => {
     }
 
     const videoId = ytdl.getURLVideoID(url)
+
     if (!videoId) {
         return response.status(400).json({ error: 'Video ID not found!' })
     }
+
     const file = ytdl(url, { filter: "audioonly" }).pipe(response)
 
     if (file.statusCode !== 200) {
         return response.status(400).json({ error: 'Video download failed' })
     }
+})
+
+app.get('/info', async (request: Request, response: Response) => {
+    const { url } = request.query as any
+
+    if (!url || undefined) {
+        return response.status(400).json({ error: 'url not found!' })
+    }
+
+    const videoId = ytdl.getURLVideoID(url)
+
+    if (!videoId) {
+        return response.status(400).json({ error: 'Video ID not found!' })
+    }
+
+    const info = await ytdl.getInfo(videoId)
+
+    return response.status(200).json({ title: info.videoDetails.title })
 })
 
 app.listen(process.env.PORT || 3000, () => {
